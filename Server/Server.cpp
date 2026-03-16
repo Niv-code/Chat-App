3e611e5bd3ceb8ea61a423b200fd6cc16c5db94f
Niv-code <nivhor0@gmail.com>
2026-03-16 16:30:16 +0200

    2nd

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,56 +1,142 @@
 #include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <memory>
+#include <array>
 
 #pragma comment(lib, "ws2_32.lib")
 
-int main() 
-{
-    WSADATA wsaData;
-    WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+std::string getGeminiResponse(const std::string& userMessage) {
     
-    sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(54321);
+    std::string apiKey = "AIzaSyBNpHQomCwftI9tfOhHMil5sRAut-QvFow"; 
+    std::string url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + apiKey;
 
-    bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
-    listen(serverSocket, SOMAXCONN);
+    std::string safeMessage = userMessage;
+    size_t pos = 0;
+    while ((pos = safeMessage.find("\"", pos)) != std::string::npos) {
+         safeMessage.replace(pos, 1, "\\\"");
+         pos += 2;
+    }
+    pos = 0;
+    while ((pos = safeMessage.find("\n", pos)) != std::string::npos) {
+         safeMessage.replace(pos, 1, " ");
+         pos += 1;
+    }
 
-    std::cout << "Chat Server is running on port 54321..." << std::endl;
-    std::cout << "Waiting for a client to connect..." << std::endl;
+    std::string jsonPayload = "{\\\"contents\\\":[{\\\"parts\\\":[{\\\"text\\\":\\\"" + safeMessage + "\\\"}]}]}";
 
-    sockaddr_in clientAddr;
-    int clientSize = sizeof(clientAddr);
-    SOCKET clientSocket = accept(serverSocket, (sockaddr*)&clientAddr, &clientSize);
+    std::string command = "curl -X POST -H \"Content-Type: application/json\" -d \"" + jsonPayload + "\" \"" + url + "\" 2>&1";
 
-    std::cout << "A client has connected successfully!" << std::endl;
-
-    char buffer[1024] = {0};
-    while (true) 
+    std::array<char, 256> buffer;
+    std::string result;
+    
+    std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(command.c_str(), "r"), _pclose);
+    if (!pipe) {
+        return "System: Failed to connect to internet.";
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) 
     {
-        int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+        result += buffer.data();
+    }
+
+    std::string searchKey = "\"text\": \"";
+    size_t startPos = result.find(searchKey);
+    if (startPos != std::string::npos) {
+        startPos += searchKey.length();
+        size_t endPos = result.find("\"", startPos);
+
+        while (endPos != std::string::npos && result[endPos - 1] == '\\') {
+            endPos = result.find("\"", endPos + 1);
+        }
+
+        if (endPos != std::string::npos) {
+            std::string reply = result.substr(startPos, endPos - startPos);
+            
+            
+            size_t n_pos = 0;
+            while((n_pos = reply.find("\\n", n_pos)) != std::string::npos) {
+                reply.replace(n_pos, 2, "\n");
+                n_pos += 1;
+            }
+            
+            n_pos = 0;
+            while((n_pos = reply.find("\\\"", n_pos)) != std::string::npos) {
+                reply.replace(n_pos, 2, "\"");
+                n_pos += 1;
+            }
+
+            return "Gemini: " + reply;
+        }
+    }
+
+    return "System Error: Could not understand Gemini's response.";
+}
+
+void handleClient(SOCKET clientSocket) {
+    char buffer[4096];
+      
+    std::string welcomeMsg = "Gemini: Hello! I'm connected. Ask me anything.";
+    send(clientSocket, welcomeMsg.c_str(), welcomeMsg.size() + 1, 0);
+
+    while (true) {
+        ZeroMemory(buffer, 4096);
+        int bytesReceived = recv(clientSocket, buffer, 4096, 0);
         
-        if (bytesReceived <= 0) 
-        {
+        if (bytesReceived <= 0) {
             std::cout << "Client disconnected." << std::endl;
             break;
         }
 
-        std::cout << "Message from client: " << buffer << std::endl;
+        std::string receivedMessage(buffer, 0, bytesReceived);
+        std::cout << "User asked: " << receivedMessage << std::endl;
+
         
-        const char* response = "Message received by server!";
-        send(clientSocket, response, strlen(response), 0);
+        std::string aiReply = getGeminiResponse(receivedMessage);
         
-        memset(buffer, 0, sizeof(buffer)); 
+        
+        send(clientSocket, aiReply.c_str(), aiReply.size() + 1, 0);
+    }
+    
+    closesocket(clientSocket);
+}
+
+int main() {
+    WSADATA wsData;
+    WORD ver = MAKEWORD(2, 2);
+    if (WSAStartup(ver, &wsData) != 0) return 1;
+
+    SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+    if (listening == INVALID_SOCKET) return 1;
+
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(54321); 
+    hint.sin_addr.S_un.S_addr = INADDR_ANY; 
+
+    bind(listening, (sockaddr*)&hint, sizeof(hint));
+    listen(listening, SOMAXCONN);
+
+    std::cout << "Gemini AI Server is running on port 54321..." << std::endl;
+
+    std::vector<std::thread> clientThreads;
+
+    while (true) {
+        sockaddr_in client;
+        int clientSize = sizeof(client);
+        SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+        
+        if (clientSocket != INVALID_SOCKET) {
+            std::cout << "New client connected to Gemini!" << std::endl;
+            clientThreads.push_back(std::thread(handleClient, clientSocket));
+            clientThreads.back().detach(); 
+        }
     }
 
-    closesocket(clientSocket);
-    closesocket(serverSocket);
+    closesocket(listening);
     WSACleanup();
-    
-    std::cout << "Server shutting down." << std::endl;
     return 0;
 }
